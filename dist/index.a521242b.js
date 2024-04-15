44c8722dba9ab9527bdaa6a91cbde04e0a751799
momani <moh.mom@ni.com>
2024-04-15 23:43:56 +0300

    second day

--- dist/index.a521242b.js
+++ dist/index.a521242b.js
@@ -587,7 +587,34 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 var _auto = require("chart.js/auto");
 var _autoDefault = parcelHelpers.interopDefault(_auto);
+// reading json
+// fetch("/28.json")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+// const sample = require('../28.json');
+// const fs = require('fs');
+// // Asynchronous read
+// fs.readFile('../28.json', 'utf-8', (err, data1) => {
+//     if (err) {
+//       console.error('Error reading file:', err);
+//       return;
+//     }
+//     const jsonData = JSON.parse(data1);
+// });
 (async function() {
+    //******************************** */
+    // fetch('../28.json') // Replace 'example.json' with the path to your JSON file
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     // 'data' contains the parsed JSON object
+    //     // You can assign it to a variable or use it as needed
+    //     const jsonData = data;
+    //     console.log(jsonData); // or do something else with jsonData
+    //   })
+    //   .catch(error => {
+    //     console.error('Error fetching JSON:', error);
+    //   });
+    //*************************************** */
     const data = [
         {
             year: 2010,
@@ -616,16 +643,65 @@ var _autoDefault = parcelHelpers.interopDefault(_auto);
         {
             year: 2016,
             count: 28
+        },
+        {
+            year: 2017,
+            count: 28
+        }
+    ];
+    const data2 = [
+        {
+            year: 2010,
+            count: 5
+        },
+        {
+            year: 2011,
+            count: 3
+        },
+        {
+            year: 2012,
+            count: 77
+        },
+        {
+            year: 2013,
+            count: 33
+        },
+        {
+            year: 2014,
+            count: 11
+        },
+        {
+            year: 2015,
+            count: 30
+        },
+        {
+            year: 2016,
+            count: 11
         }
     ];
     new (0, _autoDefault.default)(document.getElementById("acquisitions"), {
-        type: "bar",
+        type: "line",
+        options: {
+            animation: false,
+            plugins: {
+                legend: {
+                    display: false
+                },
+                tooltip: {
+                    enabled: false
+                }
+            }
+        },
         data: {
             labels: data.map((row)=>row.year),
             datasets: [
                 {
                     label: "Acquisitions by year",
                     data: data.map((row)=>row.count)
+                },
+                {
+                    label: "Acquisitions2 by year",
+                    data: data2.map((row)=>row.count)
                 }
             ]
         }
--- src/acquisitions.js
+++ src/acquisitions.js
@@ -1,6 +1,44 @@
-import Chart from 'chart.js/auto'
+import Chart from "chart.js/auto";
 
-(async function() {
+// reading json
+
+// fetch("/28.json")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+
+// const sample = require('../28.json');
+
+// const fs = require('fs');
+
+// // Asynchronous read
+// fs.readFile('../28.json', 'utf-8', (err, data1) => {
+//     if (err) {
+//       console.error('Error reading file:', err);
+//       return;
+//     }
+//     const jsonData = JSON.parse(data1);
+// });
+
+(async function () {
+
+
+//******************************** */
+// fetch('../28.json') // Replace 'example.json' with the path to your JSON file
+//   .then(response => response.json())
+//   .then(data => {
+//     // 'data' contains the parsed JSON object
+//     // You can assign it to a variable or use it as needed
+//     const jsonData = data;
+//     console.log(jsonData); // or do something else with jsonData
+//   })
+//   .catch(error => {
+//     console.error('Error fetching JSON:', error);
+//   });
+
+
+//*************************************** */
+
+
   const data = [
     { year: 2010, count: 10 },
     { year: 2011, count: 20 },
@@ -9,21 +47,50 @@ import Chart from 'chart.js/auto'
     { year: 2014, count: 22 },
     { year: 2015, count: 30 },
     { year: 2016, count: 28 },
+    { year: 2017, count: 28 },
   ];
 
-  new Chart(
-    document.getElementById('acquisitions'),
-    {
-      type: 'bar',
-      data: {
-        labels: data.map(row => row.year),
-        datasets: [
-          {
-            label: 'Acquisitions by year',
-            data: data.map(row => row.count)
-          }
-        ]
-      }
-    }
-  );
+  const data2 = [
+    { year: 2010, count: 5 },
+    { year: 2011, count: 3 },
+    { year: 2012, count: 77 },
+    { year: 2013, count: 33 },
+    { year: 2014, count: 11 },
+    { year: 2015, count: 30 },
+    { year: 2016, count: 11 },  
+  ];
+
+  new Chart(document.getElementById("acquisitions"), {
+    type: "line",
+    options: {
+      animation: false,
+      plugins: {
+        legend: {
+          display: false,
+        },
+        tooltip: {
+          enabled: false,
+        },
+      },
+    },
+    data: {
+      labels: data.map((row) => row.year),
+      datasets: [
+        {
+          label: "Acquisitions by year",
+          data: data.map((row) => row.count),
+        },
+
+        {
+            label: "Acquisitions2 by year",
+            data: data2.map((row) => row.count),
+          },
+
+        //   {
+        //     label: "Acquisitions2 by year",
+        //     data: jsonData.map((row) => row.count),
+        //   },
+          ],
+    },
+  });
 })();
